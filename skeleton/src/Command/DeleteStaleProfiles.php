6164487778f02abf8ffbbfa6f24c904d3e062f65
Grégoire Hébert <gregoire@les-tilleuls.coop>
2018-05-02 11:38:40 +0200

    Configure Command

--- skeleton/src/Command/DeleteStaleProfiles.php
+++ skeleton/src/Command/DeleteStaleProfiles.php
@@ -6,16 +6,28 @@ namespace App\Command;
 
 //use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 //class DeleteStaleProfiles extends ContainerAwareCommand
 class DeleteStaleProfiles extends Command
 {
     protected function configure()
     {
-        // ...
+        $this
+            // the name of the command (the part after "bin/console")
+            ->setName('app:profile:delete-stale')
+
+            // the short description shown while running "php bin/console list"
+            ->setDescription('Delete every profiles without any connection within 1 year.')
+
+            // the full command description shown when running the command with
+            // the "--help" option
+            ->setHelp('This command should be used at least once a week. If a profile is deleted, remember the user has had warnings.')
+        ;
     }
 
-    protected function execute()
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
         // ...
     }
