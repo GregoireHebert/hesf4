bdb2e7b03e80e389fc4db805ab0da1888ce5cbd8
Grégoire Hébert <gregoire@les-tilleuls.coop>
2018-05-02 11:29:06 +0200

    Declare a command

---
+++ skeleton/src/Command/DeleteStaleProfiles.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+//use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
+
+//class DeleteStaleProfiles extends ContainerAwareCommand
+class DeleteStaleProfiles extends Command
+{
+    protected function configure()
+    {
+        // ...
+    }
+
+    protected function execute()
+    {
+        // ...
+    }
+}
