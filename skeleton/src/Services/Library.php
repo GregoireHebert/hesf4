d69bbcb468310101de21bb0b8177ec0684561607
Grégoire Hébert <gregoire@les-tilleuls.coop>
2018-05-18 16:06:26 +0200

    create a new service

---
+++ skeleton/src/Services/Library.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+/**
+ * @author Grégoire Hébert <gregoire@les-tilleuls.coop>
+ */
+class Library
+{
+    // this is a service because autowiring automatically register it.
+    // run docker-compose exec app bin/console debug:container Library
+}
